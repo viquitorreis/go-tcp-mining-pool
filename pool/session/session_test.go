979e958d71aff21ed680f982a339272dd5969e2a
victor <victorreisprog@gmail.com>
2026-02-27 00:31:00 -0300

    fix: remove add from tests

--- pool/session/session_test.go
+++ pool/session/session_test.go
@@ -19,7 +19,6 @@ func TestSession_ConcurrentWrites_NoCorruption(t *testing.T) {
 	var wg sync.WaitGroup
 	for range 50 {
 		wg.Go(func() {
-			defer wg.Done()
 			sess.Write([]byte(`{"id":1,"result":true}` + "\n"))
 		})
 	}
